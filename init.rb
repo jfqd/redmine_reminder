b750df10fa6ab1266d4f6c7dfef829259152cd38
jfqd <jfqd@blun.org>
2013-06-18 11:06:30 +0200

    update mailer delivery for rails 3.2

--- init.rb
+++ init.rb
@@ -5,5 +5,6 @@ Redmine::Plugin.register :redmine_reminder do
   name 'Redmine reminder plugin'
   author 'Stefan Husch'
   description 'Redmine reminder plugin for netz98.de'
-  version '0.0.1'
+  version '0.0.2'
+  requires_redmine :version_or_higher => '2.3.0'
 end
