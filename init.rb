4a42991a8a39be9a83fa526289173a5282e98d35
jfqd <jfqd@blun.org>
2013-06-24 11:54:55 +0200

    add settings pannel for sms_gateway

--- init.rb
+++ init.rb
@@ -9,4 +9,12 @@ Redmine::Plugin.register :redmine_reminder do
   description 'Redmine reminder plugin for netz98.de'
   version '0.0.2'
   requires_redmine :version_or_higher => '2.3.0'
+  
+  settings :default => {
+    'sms_gateway_url' => 'https://smsserver.mindmatics.com/messagegateway/',
+    'sms_gateway_uid' => 'my user id',
+    'sms_gateway_pwd' => 'my user pwd',
+    'sms_gateway_sender_phone_number' => '0049170987654321'
+  }, :partial => 'settings/sms_gateway'
+  
 end
